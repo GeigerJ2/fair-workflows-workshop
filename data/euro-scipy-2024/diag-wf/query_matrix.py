215235013e7a7ae3136d464816daf774d66f28d8
Julian Geiger <julian.geiger@psi.ch>
2024-08-21 10:55:25 +0200

    Added compiled Linux binary of diager-rs and fixed some bugs in src

--- data/euro-scipy-2024/diag-wf/query_matrix.py
+++ data/euro-scipy-2024/diag-wf/query_matrix.py
@@ -1,33 +1,36 @@
+import argparse
 import sqlite3
 from pathlib import Path
+
 import numpy as np
 
-import argparse
-
-
-
 parser = argparse.ArgumentParser(
-                    prog='Query npy matrix from a database.',
-                    description='Queries a matrix from a matrix database.')
-parser.add_argument('db_path', help="Path of the database to query from.")
-parser.add_argument('pk', help="The primary key of the matrix to query from.")
+    prog="Query npy matrix from a database.",
+    description="Queries a matrix from a matrix database.",
+)
+parser.add_argument("db_path", help="Path of the database to query from.")
+parser.add_argument("pk", help="The primary key of the matrix to query from.")
+
 
 def query_matrix(database_path: Path, pk: int):
-    """
-    """
+    """ """
     sqlite3.register_converter("NP_NDARRAY", np.frombuffer)
     with sqlite3.connect(database_path, detect_types=sqlite3.PARSE_DECLTYPES) as conn:
         cur = conn.cursor()
-        cur.execute("SELECT pk, arr, arr_rows, arr_cols FROM matrices WHERE pk = ?", (pk,))
+        cur.execute(
+            "SELECT pk, arr, arr_rows, arr_cols FROM matrices WHERE pk = ?", (pk,)
+        )
         result = cur.fetchone()
     if result is None:
-        raise ValueError(f"No matrix with pk {pk} is available in database {database_path}")
+        raise ValueError(
+            f"No matrix with pk {pk} is available in database {database_path}"
+        )
     else:
         pk, mat, rows, cols = result
-        return mat.reshape((rows, cols))
+        return mat
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parser.parse_args()
     db_path = Path(args.db_path)
     if not db_path.exists():
@@ -36,4 +39,4 @@ if __name__ == '__main__':
         mat = query_matrix(db_path, args.pk)
         filename = f"{args.pk}"
         np.save(filename, mat)
-        print(f"Saved matrix to file {filename}")
+        print(f"Saved matrix to file {filename}.npy")
