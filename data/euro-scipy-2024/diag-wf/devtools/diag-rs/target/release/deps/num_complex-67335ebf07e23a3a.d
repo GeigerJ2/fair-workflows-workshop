9e519579fd73cee4cf1989e6b943790d8ae82852
Julian Geiger <julian.geiger@psi.ch>
2024-08-21 15:02:05 +0200

    Git remove `data/euro-scipy-2024/diag-wf/devtools/diag-rs/target/`

--- data/euro-scipy-2024/diag-wf/devtools/diag-rs/target/release/deps/num_complex-67335ebf07e23a3a.d
+++
@@ -1,9 +0,0 @@
-/home/geiger_j/aiida_projects/fair-workflows-workshop/git-repos/fair-workflows-workshop/data/euro-scipy-2024/diag-wf/devtools/diager-rs/target/release/deps/libnum_complex-67335ebf07e23a3a.rmeta: /home/geiger_j/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-complex-0.4.6/src/lib.rs /home/geiger_j/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-complex-0.4.6/src/cast.rs /home/geiger_j/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-complex-0.4.6/src/pow.rs
-
-/home/geiger_j/aiida_projects/fair-workflows-workshop/git-repos/fair-workflows-workshop/data/euro-scipy-2024/diag-wf/devtools/diager-rs/target/release/deps/libnum_complex-67335ebf07e23a3a.rlib: /home/geiger_j/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-complex-0.4.6/src/lib.rs /home/geiger_j/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-complex-0.4.6/src/cast.rs /home/geiger_j/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-complex-0.4.6/src/pow.rs
-
-/home/geiger_j/aiida_projects/fair-workflows-workshop/git-repos/fair-workflows-workshop/data/euro-scipy-2024/diag-wf/devtools/diager-rs/target/release/deps/num_complex-67335ebf07e23a3a.d: /home/geiger_j/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-complex-0.4.6/src/lib.rs /home/geiger_j/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-complex-0.4.6/src/cast.rs /home/geiger_j/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-complex-0.4.6/src/pow.rs
-
-/home/geiger_j/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-complex-0.4.6/src/lib.rs:
-/home/geiger_j/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-complex-0.4.6/src/cast.rs:
-/home/geiger_j/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-complex-0.4.6/src/pow.rs:
