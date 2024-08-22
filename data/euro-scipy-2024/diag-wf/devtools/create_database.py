60c6e9f9d30577c7cee95ad79909f2c6bb81c3ac
Ali <khsrali@gmail.com>
2024-08-22 09:03:21 +0200

    update on: create_database.py

--- data/euro-scipy-2024/diag-wf/devtools/create_database.py
+++ data/euro-scipy-2024/diag-wf/devtools/create_database.py
@@ -9,6 +9,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--db_name', default="matrices.db", help="Name of the database.")
 parser.add_argument('--nb_mats', default=100, help="Number of matrices to populate the database with.", required=False)
 parser.add_argument('--dim_mats', default=50, help="Dimension of the matrices.")
+parser.add_argument('--offset', default=0, help="Offset in dimension of the matrices.")
 
 def adapt_array(arr):
     """
@@ -24,7 +25,7 @@ def convert_array(arr_text):
     out.seek(0)
     return np.load(out)
 
-def create_sqlite_database(database_filename: str, nb_mats: int, dim_mats):
+def create_sqlite_database(database_filename: str, nb_mats: int, dim_mats : int, offset : int):
     """ create a database connection to an SQLite database """
     sqlite3.register_adapter(np.ndarray, lambda x: x.tobytes())
     sqlite3.register_converter("NP_NDARRAY", np.frombuffer)
@@ -40,13 +41,13 @@ def create_sqlite_database(database_filename: str, nb_mats: int, dim_mats):
 
         rng = np.random.default_rng(seed=0)
         for pk in range(nb_mats):
-            mat = rng.random((dim_mats, dim_mats))
+            offset_ = np.random.randint(-offset, offset)
+            mat = rng.random((dim_mats+offset_, dim_mats+offset_))
 
             cur.execute("INSERT INTO matrices (pk, arr, arr_rows, arr_cols) VALUES (?, ?, ?, ?)",
                         (pk, mat, mat.shape[0], mat.shape[1]))
 
 
-
 if __name__ == '__main__':
     args = parser.parse_args()
-    create_sqlite_database(args.db_name, args.nb_mats, args.dim_mats)
+    create_sqlite_database(args.db_name, int(args.nb_mats), int(args.dim_mats), int(args.offset))
